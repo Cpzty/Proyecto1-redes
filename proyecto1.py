d20121c55f24da1e81e7390ac201ead2e979b49f
Cpzty <Cristianpzty@gmail.com>
2019-07-30 17:44:14 -0600

    send files

--- proyecto1.py
+++ proyecto1.py
@@ -18,8 +18,16 @@ else:
     raw_input = input
 #client class
 class ChatBot(sleekxmpp.ClientXMPP):
-    def __init__(self, jid, password,):
+    def __init__(self, jid, password, room, nick):
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        #file send plugin
+        self.register_plugin('xep_0047', {
+            'auto_accept': True
+        })
+        #room
+        self.room = room
+        #nickname
+        self.nick = nick
         #recipient
         #self.recipient = recipient
         #message
@@ -28,6 +36,13 @@ class ChatBot(sleekxmpp.ClientXMPP):
         self.add_event_handler("session_start", self.start, threaded=True)
         #register
         self.add_event_handler("register", self.register, threaded=True)
+        #group chat message
+        self.add_event_handler("groupchat_message", self.muc_message, threaded=True)
+        #group chat presence
+        self.add_event_handler("muc::%s::got_online" % self.room, self.muc_online, threaded=True)
+        #file send handlers
+        self.add_event_handler("ibb_stream_start", self.stream_opened, threaded=True)
+        self.add_event_handler("ibb_stream_data", self.stream_data)
 
     def start(self, event):
           self.send_presence()
@@ -61,6 +76,7 @@ class ChatBot(sleekxmpp.ClientXMPP):
         resp['register'] = ' '
         resp['register']['remove'] = ' '
         try:
+            print(resp)
             resp.send(now=True)
             print("Account deleted for %s!" % self.boundjid)
         except IqError as e:
@@ -74,6 +90,34 @@ class ChatBot(sleekxmpp.ClientXMPP):
     def message(self, msg):
         if msg['type'] in ('chat', 'normal'):
             print ("%(body)s" % msg)
+
+    def muc_message(self, msg):
+            if msg['mucnick'] != self.nick and self.nick in msg['body']:
+                print ("%(body)s" % msg)
+
+    def muc_online(self, presence):
+            if presence['muc']['nick'] != self.nick:
+                self.send_message(mto=presence['from'].bare,
+                mbody="Hello, %s %s" % (presence['muc']['role'],
+                presence['muc']['nick']),
+                mtype='groupchat')
+
+    def accept_stream(self, iq):
+            return True
+
+    def stream_opened(self, stream):
+        print('Stream opened: %s from %s' % (stream.sid, stream.peer_jid))
+
+    def stream_data(self, event):
+            print(event['data'])
+
+    def send_filerino(self,whotosend, filetosend):
+            stream = self['xep_0047'].open_stream(whotosend)
+        
+            with open(filetosend) as f:
+                data = f.read()
+                stream.sendall(data)
+            
 
 if __name__ == '__main__':
         optp = OptionParser()
@@ -104,6 +148,12 @@ if __name__ == '__main__':
         optp.add_option("-m", "--message", dest="message",
                     help="message to send")
 
+        optp.add_option("-r", "--room", dest="room",
+                    help="MUC room to join")
+        optp.add_option("-n", "--nick", dest="nick",
+                    help="MUC nickname")
+
+
         opts, args = optp.parse_args()
 
         #logging config
@@ -128,8 +178,10 @@ if __name__ == '__main__':
                 
         opts.jid = raw_input("Username: ")
         opts.password = getpass.getpass("Password: ")
+        opts.nick = raw_input("Nickname: ")
+        opts.room = raw_input("Room to join: ")
         
-        xmpp = ChatBot(opts.jid, opts.password,)
+        xmpp = ChatBot(opts.jid, opts.password, opts.nick, opts.room)
 
         if(login_register == str(2)):
                 xmpp.del_event_handler("register", xmpp.register)
@@ -143,6 +195,7 @@ if __name__ == '__main__':
         xmpp.register_plugin('xep_0066') # Out-of-band Data
         xmpp.register_plugin('xep_0077') # In-band Registration
         xmpp.register_plugin('xep_0045') # multichat
+        #xmpp.register_plugin('xep_0047') # file
 
         
         #authentication over an unencrypted connection
@@ -166,6 +219,7 @@ if __name__ == '__main__':
                         print("press 8 to send a message")
                         #may fuse with send message
                         print("press 9 to send a file")
+                        print("press 10 for group message")
                         ch = raw_input(">: ")
                 #disconnect
                         if(ch == str(1)):
@@ -176,6 +230,7 @@ if __name__ == '__main__':
                         elif(ch == str(2)):
                                 xmpp.remove_user()
                                 xmpp.disconnect()
+                                break
                 #see all contacts
                         elif(ch == str(3)):
                                 print("showing all contacts\n")
@@ -187,7 +242,9 @@ if __name__ == '__main__':
                                 friend_request = raw_input(":> ")
                                 xmpp.send_presence(pto=friend_request, ptype='subscribe')
                 #join a room
-                        #elif(ch == str(6)):
+                        elif(ch == str(6)):
+                                print("joining room")
+                                xmpp.plugin['xep_0045'].joinMUC(xmpp.room, xmpp.nick)
                                 
                 #presence message
                         elif(ch == str(7)):
@@ -205,6 +262,25 @@ if __name__ == '__main__':
                                 print("sending msg")
                                 xmpp.send_message(mto=user_to_send, mbody = msg_to_send, mtype = 'chat')
                                 #xmpp.message(msg.type='chat')
+                #send file
+                        elif(ch==str(9)):
+                                print("Who would u like to send a file to?")
+                                file_receiver = raw_input(">: ")
+                                print("What is the name file?")
+                                file_to_send = raw_input(">: ")
+                                #stream = xmpp['xep_0047'].open_stream(file_receiver)
+                                xmpp.send_filerino(file_receiver, file_to_send)
+                                #with open(file_to_send) as f:
+                                  #      data = f.read()
+                                    #    stream.sendall(data)
+                                
+                #group message
+                        elif(ch==str(10)):
+                                print("what would u like to send?")
+                                grpmsg = raw_input(">: ")
+                                xmpp.send_message(mto='all',
+                                mbody=grpmsg,
+                                mtype='groupchat')
 
         else:
                 print("Unable to connect.")
